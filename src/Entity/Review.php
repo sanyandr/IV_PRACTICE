3566ee16984cae5603bc9efc4c535ef889110a09
Sanyandr <alexenderboot@gmail.com>
2023-02-04 02:43:17 +0300

    commit 04.02 02:43 Review constructor now implements \JsonSerializable, json_encode has access to certain private fields

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -1,14 +1,27 @@
 <?php
 namespace Sanyandr\Practice\Entity;
 
-class Review {
-    public $ID = 0;
-    public $text = "";
-    public $rate = 0;
-    public $userID = 0;
+class Review implements \JsonSerializable {
+    protected $ID;
+    protected $text;
+    protected $rate;
+    protected $userID;
 
-    public function __construct() {
+    public function __construct($ID, $text, $rate, $userID) {
+        $this->ID = $ID;
+        $this->text = $text;
+        $this->rate = $rate;
+        $this->userID = $userID;
+
     }
 
 
+    public function jsonSerialize(): mixed {
+        return [
+            'ID' => $this->ID,
+            'text' => $this->text,
+            'rate' => $this->rate,
+            'userID' => $this->userID,
+        ];
+    }
 }
